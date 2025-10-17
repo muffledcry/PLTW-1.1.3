93484be23d7e42be3253eca3eac4c9f9728ce04f
muffledcry <59100054+muffledcry@users.noreply.github.com>
2025-10-17 12:02:53 -0400

    Create your_mom.js

---
+++ your_mom.js
@@ -0,0 +1,12 @@
+color = input()
+
+if color == "red":
+  print("The color is red.")
+elif color == "yellow":
+  print(35 + 39)
+elif color == "green":
+  print("True")
+elif color == "purple":
+  print("Give me an A!")
+else:
+  print("Not a valid color.")
